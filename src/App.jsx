e77400c47902b3b3d3c328dd2b82576d0e3c2a34
Your_Name Your_Surname <your@email>
2024-11-01 16:03:52 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
 import { FilterMovies } from './components/FilterMovies/FilterMovies';
 
+function findMovies(movies, { query }) {
+  const normalizedQuery = query.trim().toLowerCase();
+
+  return movies.filter(
+    movie =>
+      movie.title.toLowerCase().includes(normalizedQuery) ||
+      movie.description.toLowerCase().includes(normalizedQuery),
+  );
+}
+
 export const App = () => {
   const [query, setQuery] = useState('');
-
-  function findMovies(movies, { queris }) {
-    const normalizedQuery = queris.trim().toLowerCase();
-
-    return movies.filter(
-      movie =>
-        movie.title.toLowerCase().includes(normalizedQuery) ||
-        movie.description.toLowerCase().includes(normalizedQuery),
-    );
-  }
 
   const visibleMovies = findMovies(moviesFromServer, { query });
 
